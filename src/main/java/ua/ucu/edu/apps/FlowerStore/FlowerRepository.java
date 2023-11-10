b36765527f21a725cb0669977615f81f85bc4e0a
petrunivyaryna <petruniv.pn@ucu.edu.ua>
2023-11-10 11:36:30 +0200

    changes

--- src/main/java/ua/ucu/edu/apps/FlowerStore/FlowerRepository.java
+++
@@ -1,7 +0,0 @@
-package ua.ucu.edu.apps.flowerstore;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FlowerRepository extends JpaRepository<Flower, Integer> {
-    
-}
--- src/main/java/ua/ucu/edu/apps/flowerstore/FlowerController.java
+++ src/main/java/ua/ucu/edu/apps/flowerstore/FlowerController.java
@@ -2,10 +2,7 @@ package ua.ucu.edu.apps.flowerstore;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
